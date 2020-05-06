68ae1348da10dc3ded455c04a06230af703c9cfd
vilmibm <vilmibm@neongrid.space>
2020-05-06 16:25:20 -0500

    stubs

--- command/pr_review_test.go
+++ command/pr_review_test.go
@@ -14,3 +14,12 @@ func TestPRReview_validation(t *testing.T) {
 		eq(t, err.Error(), "need exactly one of approve, request-changes, or comment")
 	}
 }
+
+func TestPRReview_url_arg(t *testing.T) {
+}
+
+func TestPRReview_number_arg(t *testing.T) {
+}
+
+func TestPRReview_no_arg(t *testing.T) {
+}
